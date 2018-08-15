604f4c8369fbbbdbec27b4dffdfdcc0025d9c8f0
Sergey Tepliakov <seteplia@microsoft.com>
2018-08-15 16:45:59 -0700

    Fail gracefully if no analyzers are found.

--- src/Apps/ErrorProne.NET.Cli/Configuration.cs
+++ src/Apps/ErrorProne.NET.Cli/Configuration.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Diagnostics.Contracts;
 using System.IO;
 using System.Reflection;
