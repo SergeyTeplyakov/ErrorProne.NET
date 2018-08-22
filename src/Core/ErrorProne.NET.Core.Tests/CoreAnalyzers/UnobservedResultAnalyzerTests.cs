53c8db031cede7e667d20ff04963fd77e19354f1
Sergey Tepliakov <seteplia@microsoft.com>
2018-08-22 11:37:05 -0700

    More robust UnobservedResult analyzer.

--- src/Core/ErrorProne.NET.Core.Tests/CoreAnalyzers/UnobservedResultAnalyzerTests.cs
+++ src/Core/ErrorProne.NET.Core.Tests/CoreAnalyzers/UnobservedResultAnalyzerTests.cs
@@ -46,6 +46,124 @@ class FooBar
         }
 
         [Test]
+        public void Result_That_Flows_Through_Extension_Method_Is_Observed()
+        {
+            string code = @"
+class Result {}
+static class ResultEx {
+    public static Result Handle(this Result r) => r;
+}
+class FooBar
+{
+    public static Result Foo() => null;
+
+    public static void Test()
+    {
+        Foo().Handle();
+    }
+}
+";
+            NoDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void Result_That_Flows_Through_Extension_Method_Is_Observed_For_Tasks()
+        {
+            string code = @"
+class Result {}
+static class ResultEx {
+    public static async System.Threading.Tasks.Task<Result> Handle(this System.Threading.Tasks.Task<Result> r) => r;
+}
+class FooBar
+{
+    public static async System.Threading.Tasks.Task<Result> Foo() => null;
+
+    public static async Task Test()
+    {
+        await Foo().Handle();
+    }
+}
+";
+            NoDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void Method_That_Returns_Result_Should_Be_Observed_In_Await()
+        {
+            string code = @"
+class Result {}
+
+class FooBar
+{
+    public static System.Threading.Tasks.Task<Result> Foo() => null;
+
+    public static async System.Threading.Tasks.Task Test()
+    {
+        [|await Foo()|];
+    }
+}
+";
+            HasDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void Method_That_Returns_Result_Should_Be_Observed_In_Await_With_ConfigureAwait()
+        {
+            string code = @"
+class Result {}
+
+class FooBar
+{
+    public static System.Threading.Tasks.Task<Result> Foo() => null;
+
+    public static async System.Threading.Tasks.Task Test()
+    {
+        [|await Foo().ConfigureAwait(false)|];
+    }
+}
+";
+            HasDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void Warn_On_Awaiting_The_Task_That_Returns_Result()
+        {
+            string code = @"
+class Result {}
+
+class FooBar
+{
+    public static System.Threading.Tasks.Task<Result> Foo => null;
+
+    public static async System.Threading.Tasks.Task Test()
+    {
+        [|await Foo.ConfigureAwait(false)|];
+    }
+}
+";
+            HasDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void Warn_On_Awaiting_The_Task_That_Returns_Result_Without_ConfigureAwait()
+        {
+            string code = @"
+class Result {}
+
+class FooBar
+{
+    public static System.Threading.Tasks.Task<Result> Foo => null;
+
+    public static async System.Threading.Tasks.Task Test()
+    {
+        [|await Foo|];
+    }
+}
+";
+            HasDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
         public void Method_That_Returns_ResultBase_Should_Be_Observed()
         {
             string code = @"
@@ -82,6 +200,21 @@ class FooBar
 }
 ";
             HasDiagnostic(code, DiagnosticId);
+        }
+
+        [Test]
+        public void NoWarnings_For_Tasks_ContinueWith()
+        {
+            string code = @"
+class FooBar
+{
+    public static void Test(System.Threading.Tasks.Task task)
+    {
+        task.ContinueWith(t => {});
+    }
+}
+";
+            NoDiagnostic(code, DiagnosticId);
         }
     }
 }
--- src/Core/ErrorProne.NET.Core/CoreAnalyzers/UnobservedResultAnalyzer.cs
+++ src/Core/ErrorProne.NET.Core/CoreAnalyzers/UnobservedResultAnalyzer.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Threading.Tasks;
 using ErrorProne.NET.CoreAnalyzers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace ErrorProne.NET.Core.CoreAnalyzers
 {
@@ -30,7 +32,7 @@ namespace ErrorProne.NET.Core.CoreAnalyzers
 
         /// <nodoc />
         private static readonly DiagnosticDescriptor Rule =
-            new DiagnosticDescriptor(DiagnosticId, Title, Title, Category, Severity, isEnabledByDefault: true, description: Description);
+            new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, isEnabledByDefault: true, description: Description);
 
         /// <nodoc />
         public UnobservedResultAnalyzer() 
@@ -41,6 +43,7 @@ namespace ErrorProne.NET.Core.CoreAnalyzers
         /// <inheritdoc />
         public override void Initialize(AnalysisContext context)
         {
+            context.RegisterSyntaxNodeAction(AnalyzeAwaitExpression, SyntaxKind.AwaitExpression);
             context.RegisterSyntaxNodeAction(AnalyzeMethodInvocation, SyntaxKind.InvocationExpression);
         }
 
@@ -51,22 +54,109 @@ namespace ErrorProne.NET.Core.CoreAnalyzers
             // TODO: different error message should be used for UseReturnValueAttribute
             if (invocation.Parent is ExpressionStatementSyntax ex &&
                 context.SemanticModel.GetSymbolInfo(ex.Expression).Symbol is IMethodSymbol ms && 
-                TypeMustBeObserved(ms.ReturnType, context.Compilation))
+                TypeMustBeObserved(ms.ReturnType, ms, context.Compilation))
             {
-                context.ReportDiagnostic(Diagnostic.Create(Rule, invocation.GetNodeLocationForDiagnostic()));
+                if (!ResultObservedByExtensionMethod(invocation, ms, context.SemanticModel))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, invocation.GetNodeLocationForDiagnostic(), ms.ReturnType.Name);
+                    context.ReportDiagnostic(diagnostic);
+                }
             }
         }
 
-        private bool TypeMustBeObserved(ITypeSymbol type, Compilation compilation)
+        private void AnalyzeAwaitExpression(SyntaxNodeAnalysisContext context)
         {
+            var awaitExpression = (AwaitExpressionSyntax)context.Node;
+
+            // await can be used on a task value, so the awaited expression may be anything.
+            if (awaitExpression.Parent is ExpressionStatementSyntax ex)
+            {
+                var operation = context.SemanticModel.GetOperation(awaitExpression);
+                if (operation is IAwaitOperation awaitOperation && operation.Type != null && TypeMustBeObserved(operation.Type, null, context.Compilation))
+                {
+                    if (awaitOperation.Operation is IInvocationOperation invocation &&
+                        ResultObservedByExtensionMethod(
+                            invocation.Syntax as InvocationExpressionSyntax,
+                            invocation.TargetMethod,
+                            context.SemanticModel))
+                    {
+                        // Result is observed!
+                        return;
+                    }
+
+                    // Need to extract a real method if this one is 'ConfigureAwait'
+                    var location = GetLocationForDiagnostic(awaitExpression);
+
+                    var diagnostic = Diagnostic.Create(Rule, location, operation.Type.Name);
+                    ReportDiagnostic(context, diagnostic);
+                }
+            }
+        }
+
+        private bool ResultObservedByExtensionMethod(InvocationExpressionSyntax methodCall, IMethodSymbol methodSymbol, SemanticModel semanticModel)
+        {
+            if (methodCall == null)
+            {
+                return false;
+            }
+
+            // In some cases, the following pattern is used:
+            // Foo().Handle();
+            // Where Foo() returns 'possible' that is passed to 'Handle' extension method that returns the result.
+            // But in this case we can safely assume that the result WAS observed.
+
+            // Exception for this rule is 'ConfigureAwait()'
+            if (methodSymbol.IsConfigureAwait(semanticModel.Compilation))
+            {
+                return false;
+            }
+
+            // First, checking that method that is called is an extension method that takes the result.
+            if (methodSymbol.IsExtensionMethod &&
+                (methodSymbol.ReturnType.Equals(methodSymbol.ReceiverType) ||
+                 methodSymbol.ReturnType.Equals(methodSymbol.Parameters.FirstOrDefault()?.Type)))
+            {
+                // Looking for 'Foo' in the example provided above
+                if (methodCall.Expression is MemberAccessExpressionSyntax mae &&
+                    mae.Expression is InvocationExpressionSyntax &&
+                    semanticModel.GetSymbolInfo(mae.Expression).Symbol is IMethodSymbol nestedMethod)
+                {
+                    return nestedMethod.ReturnType.Equals(methodSymbol.ReturnType);
+                }
+            }
+
+            return false;
+        }
+
+        private Location GetLocationForDiagnostic(AwaitExpressionSyntax awaitExpression)
+        {
+            return awaitExpression.GetLocation();
+        }
+
+        private static void ReportDiagnostic(SyntaxNodeAnalysisContext context, Diagnostic diagnostic)
+        {
+#if DEBUG
+            Console.WriteLine($"ERROR: {diagnostic}");
+#endif
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        private bool TypeMustBeObserved(ITypeSymbol type, /*CanBeNull*/IMethodSymbol ms, Compilation compilation)
+        {
+            if (ms?.IsContinueWith(compilation) == true)
+            {
+                // Task.ContinueWith is a bit special.
+                return false;
+            }
+
             return EnumerateBaseTypesAndSelf(type).Any(t => IsObserableType(t, compilation));
         }
 
         private bool IsObserableType(ITypeSymbol type, Compilation compilation)
         {
-            if (type.IsClrType(compilation, typeof(Exception)))
+            if (type.IsClrType(compilation, typeof(Exception)) || type.IsClrType(compilation, typeof(Task)))
             {
-                // Exceptions as special
+                // Exceptions and tasks are special
                 return true;
             }
 
