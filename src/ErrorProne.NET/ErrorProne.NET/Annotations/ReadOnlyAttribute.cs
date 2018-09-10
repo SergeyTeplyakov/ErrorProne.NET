bf5730f9626659621669d704f316ed608a3ce565
Gérald Barré <meziantou@users.noreply.github.com>
2018-09-10 10:43:27 -0400

    Fix typo

--- src/ErrorProne.NET/ErrorProne.NET/Annotations/ReadOnlyAttribute.cs
+++ src/ErrorProne.NET/ErrorProne.NET/Annotations/ReadOnlyAttribute.cs
@@ -6,7 +6,7 @@ namespace ErrorProne.NET.Annotations
     /// Attribute that mimics readonly modifier on fields.
     /// </summary>
     /// <remarks>
-    /// The reason of this attirubte is to get readonly behavior without paying the cost of
+    /// The reason of this attribute is to get readonly behavior without paying the cost of
     /// copying readonly fields of value types.
     /// 
     /// ErrorProne.NET will emit an error if this attribute would be applied on the field of non-custom structs.
