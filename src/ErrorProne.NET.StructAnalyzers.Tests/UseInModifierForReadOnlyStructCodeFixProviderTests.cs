b41c44628b9788e26fe4684ea982df3eb88da4eb
Sam Harwell <sam@tunnelvisionlabs.com>
2019-04-08 07:57:32 -0500

    EPS05 code fix should fix implementations and callers
    Fixes #122
    Fixes #123

--- src/ErrorProne.NET.StructAnalyzers.Tests/UseInModifierForReadOnlyStructCodeFixProviderTests.cs
+++ src/ErrorProne.NET.StructAnalyzers.Tests/UseInModifierForReadOnlyStructCodeFixProviderTests.cs
@@ -26,6 +26,204 @@ namespace ErrorProne.NET.StructAnalyzers.Tests
         }
 
         [Test]
+        public async Task AddInModifierToCallSites()
+        {
+            string code = @"
+readonly struct FooBar
+{
+    public static void Foo([|FooBar fb|]) {Foo(fb);}
+    readonly (long, long, long) data;
+}";
+
+            string expected = @"
+readonly struct FooBar
+{
+    public static void Foo(in FooBar fb) {Foo(in fb);}
+    readonly (long, long, long) data;
+}";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } },
+                FixedState = { Sources = { expected } },
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+
+        [Test]
+        public async Task AddInModifierToImplementingMethods()
+        {
+            string code = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+interface IInterface
+{
+    void Method([|FooBar fb|]);
+}
+
+class Class
+{
+    public void Method([|FooBar fb|]) => throw null;
+}
+";
+
+            string expected = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+interface IInterface
+{
+    void Method(in FooBar fb);
+}
+
+class Class
+{
+    public void Method(in FooBar fb) => throw null;
+}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } },
+                FixedState = { Sources = { expected } },
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+
+        [Test]
+        public async Task AddInModifierToExplicitImplementingMethods()
+        {
+            string code = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+interface IInterface
+{
+    void Method([|FooBar fb|]);
+}
+
+class Class : IInterface
+{
+    void IInterface.Method(FooBar fb) => throw null;
+}
+";
+
+            string expected = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+interface IInterface
+{
+    void Method(in FooBar fb);
+}
+
+class Class : IInterface
+{
+    void IInterface.Method(in FooBar fb) => throw null;
+}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } },
+                FixedState = { Sources = { expected } },
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+
+        [Test]
+        public async Task AddInModifierToOverridingMethods()
+        {
+            string code = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+abstract class BaseClass
+{
+    protected abstract void Method([|FooBar fb|]);
+}
+
+class DerivedClass : BaseClass
+{
+    protected override void Method(FooBar fb) => throw null;
+}
+";
+
+            string expected = @"
+readonly struct FooBar
+{
+    readonly (long, long, long) data;
+}
+
+abstract class BaseClass
+{
+    protected abstract void Method(in FooBar fb);
+}
+
+class DerivedClass : BaseClass
+{
+    protected override void Method(in FooBar fb) => throw null;
+}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } },
+                FixedState = { Sources = { expected } },
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+
+        [Test]
+        public async Task AddInModifierToCallSites2()
+        {
+            string code1 = @"
+readonly struct FooBar
+{
+    public static void Foo([|FooBar fb|]) { }
+    readonly (long, long, long) data;
+}";
+            string code2 = @"
+class Referencer
+{
+    FooBar _value;
+    public void Method()
+    {
+        FooBar.Foo(_value);
+    }
+}";
+
+            string expected1 = @"
+readonly struct FooBar
+{
+    public static void Foo(in FooBar fb) { }
+    readonly (long, long, long) data;
+}";
+            string expected2 = @"
+class Referencer
+{
+    FooBar _value;
+    public void Method()
+    {
+        FooBar.Foo(in _value);
+    }
+}";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code1, code2 } },
+                FixedState = { Sources = { expected1, expected2 } },
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+
+        [Test]
         public async Task AddInModifierWithTrivia()
         {
             string code = @"readonly struct FooBar
