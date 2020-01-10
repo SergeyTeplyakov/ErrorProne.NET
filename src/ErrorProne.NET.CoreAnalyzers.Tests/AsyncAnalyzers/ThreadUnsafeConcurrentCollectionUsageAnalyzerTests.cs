f95fc90d47caf659a6215c32ac274ecfa510374c
Sergey Tepliakov <Sergey.Teplyakov@gmail.com>
2020-01-10 15:57:27 -0800

    Exclude locals from the analysis.

--- src/ErrorProne.NET.CoreAnalyzers.Tests/AsyncAnalyzers/ThreadUnsafeConcurrentCollectionUsageAnalyzerTests.cs
+++ src/ErrorProne.NET.CoreAnalyzers.Tests/AsyncAnalyzers/ThreadUnsafeConcurrentCollectionUsageAnalyzerTests.cs
@@ -20,10 +20,11 @@ namespace ErrorProne.NET.CoreAnalyzers.Tests.AsyncAnalyzers
 using System.Linq;
 public class MyClass
 {
+    private static System.Collections.Concurrent.ConcurrentDictionary<int, string> cd = new System.Collections.Concurrent.ConcurrentDictionary<int, string>();
     public static void Foo()
     {
-       var cd = new System.Collections.Concurrent.ConcurrentDictionary<int, string>();
        var sequence = [|System.Linq.Enumerable.OrderBy(cd, kvp => kvp.Key)|];
+       sequence = [|cd.OrderBy(kvp => kvp.Key)|];
        sequence = [|cd.OrderByDescending(kvp => kvp.Key)|];
     }
 }";
@@ -36,6 +37,28 @@ public class MyClass
         
         [Test]
         public async Task NoWarn_On_ConcurrentDictionary_Keys_OrderBy()
+        {
+            var list = new List<int>();
+            string code = @"
+using System.Linq;
+public class MyClass
+{
+    private static System.Collections.Concurrent.ConcurrentDictionary<int, string> cd = new System.Collections.Concurrent.ConcurrentDictionary<int, string>();
+    public static void Foo()
+    {
+       var r1 = cd.Keys.OrderBy(kvp => kvp);
+       var r2 = cd.Values.ToArray();
+    }
+}";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } }
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+        
+        [Test]
+        public async Task NoWarn_On_ConcurrentDictionary_Local()
         {
             var list = new List<int>();
             string code = @"
@@ -63,13 +86,41 @@ public class MyClass
 using System.Linq;
 public class MyClass
 {
+    private static System.Collections.Concurrent.ConcurrentDictionary<int, string> cd = new System.Collections.Concurrent.ConcurrentDictionary<int, string>();
+
     public static void Foo()
     {
-       var cd = new System.Collections.Concurrent.ConcurrentDictionary<int, string>();
        var r1 = [|System.Linq.Enumerable.ToList(cd)|];
        var r2 = [|System.Linq.Enumerable.ToArray(cd)|];
        var r3 = [|cd.ToList()|];
        var r4 = cd.ToArray(); // This is an instance method, so it is fine!
+    }
+}";
+
+            await new VerifyCS.Test
+            {
+                TestState = { Sources = { code } }
+            }.WithoutGeneratedCodeVerification().RunAsync();
+        }
+        
+        [Test]
+        public async Task Warn_On_ConcurrentDictionary_DifferentSources_ToList()
+        {
+            string code = @"
+using System.Linq;
+using CD = System.Collections.Concurrent.ConcurrentDictionary<int, string>;
+public class MyClass
+{
+    private static CD field = null;
+    private static CD Prop => null;
+    private static CD Method() => null;
+
+    public static void Foo(CD arg)
+    {
+       var r1 = [|arg.ToList()|];
+       r1 = [|field.ToList()|];
+       r1 = [|Prop.ToList()|];
+       r1 = [|Method().ToList()|];
     }
 }";
 
--- src/ErrorProne.NET.CoreAnalyzers/ConcurrencyAnalyzers/ConcurrentCollectionAnalyzer.cs
+++ src/ErrorProne.NET.CoreAnalyzers/ConcurrencyAnalyzers/ConcurrentCollectionAnalyzer.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using ErrorProne.NET.CoreAnalyzers;
 using Microsoft.CodeAnalysis;
@@ -57,31 +59,40 @@ namespace ErrorProne.NET.AsyncAnalyzers
 
         private void AnalyzeInvocationOperation(OperationAnalysisContext context)
         {
-            // Detecting the calls to concurrentDictionaryInstance.OrderBy because
-            // it may fail with ArgumentException if the collection is being mutated concurrently at the same time.
-            var invocationOperation = (IInvocationOperation)context.Operation;
-
-
-            var receiverType = GetReceiverType(context.Compilation, invocationOperation);
-
-            var targetMethodName = invocationOperation.TargetMethod.Name;
-            if (receiverType != null &&
-                invocationOperation.TargetMethod.ContainingType.ToDisplayString() == "System.Linq.Enumerable" &&
-                ConcurrentDictionaryUnsafeLinqOperations.Contains(targetMethodName))
+            try
             {
-                if (receiverType.ToDisplayString().StartsWith("System.Collections.Concurrent.ConcurrentDictionary<") == true)
-                {
-                    string extra = $" Calling {invocationOperation.TargetMethod.Name} on ConcurrentDictionary is not thread-safe and may fail with ArgumentException.";
-                    
-                    // Extra hint in case ToList or Enumerable.ToArray(cd) is used: the only thread-safe alternative is to use an instance ToArray method.
-                    if (targetMethodName == "ToList" || targetMethodName == "ToArray")
-                    {
-                        extra += " Use instance ToArray() method instead.";
-                    }
+                // Detecting the calls to concurrentDictionaryInstance.OrderBy because
+                // it may fail with ArgumentException if the collection is being mutated concurrently at the same time.
+                var invocationOperation = (IInvocationOperation)context.Operation;
 
-                    var diagnostic = Diagnostic.Create(Rule, invocationOperation.Syntax.GetLocation(), extra);
-                    context.ReportDiagnostic(diagnostic);
+                var receiverType = GetReceiverType(context.Compilation, invocationOperation);
+
+                var targetMethodName = invocationOperation.TargetMethod.Name;
+                if (receiverType != null &&
+                    invocationOperation.TargetMethod.ContainingType.ToDisplayString() == "System.Linq.Enumerable" &&
+                    ConcurrentDictionaryUnsafeLinqOperations.Contains(targetMethodName))
+                {
+                    if (receiverType.ToDisplayString()
+                            .StartsWith("System.Collections.Concurrent.ConcurrentDictionary<") == true)
+                    {
+                        string extra =
+                            $" Calling '{invocationOperation.TargetMethod.Name}' on a shared ConcurrentDictionary instance is not thread-safe and may fail with ArgumentException.";
+
+                        // Extra hint in case ToList or Enumerable.ToArray(cd) is used: the only thread-safe alternative is to use an instance ToArray method.
+                        if (targetMethodName == "ToList" || targetMethodName == "ToArray")
+                        {
+                            extra += " Use instance ToArray() method instead.";
+                        }
+
+                        var diagnostic = Diagnostic.Create(Rule, invocationOperation.Syntax.GetLocation(), extra);
+                        context.ReportDiagnostic(diagnostic);
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                //Debugger.Launch();
+                throw new Exception(e.StackTrace);
             }
         }
 
@@ -99,7 +110,7 @@ namespace ErrorProne.NET.AsyncAnalyzers
             var semanticModel = compilation.GetSemanticModel(firstArg.Syntax.SyntaxTree);
             var argumentOperation = semanticModel.GetOperation(firstArg.Syntax);
 
-            if (argumentOperation is IArgumentOperation ao)
+            if (argumentOperation is IArgumentOperation)
             {
                 // This is the same argument operation that we obtained before.
                 // It means that this is a real argument like Enumerable.ToList(arg)
@@ -115,12 +126,20 @@ namespace ErrorProne.NET.AsyncAnalyzers
                 return semanticModel.GetTypeInfo(argumentIdentifier).Type;
             }
 
-            if (argumentOperation is ILocalReferenceOperation lro)
+            // This is 'foo.ToList()' case.
+            // Just getting a type of an operation but we need to exclude locals.
+
+            if (argumentOperation is ILocalReferenceOperation)
             {
-                return lro.Type;
+                // Important NOTE: excluding local variables, because it is way likely that the local is used in a shared context.
+                // In most cases the locals are used in some kind of fork-join scenario, when the local is created,
+                // populated in parallel via Parallel.For or something similar and then processed after that.
+                // It is still possible that this code may cause issues, but because this pattern is relatively popular
+                // its better to avoid false positives here.
+                return null;
             }
 
-            return null;
+            return argumentOperation?.Type;
         }
     }
 }
