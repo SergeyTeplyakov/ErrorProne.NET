528b84980f89089c0ab06a59d3d66d62e363937e
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2020-05-13 23:14:47 -0700

    Fix issue with readonly structs like ReadOnlyMemory

---
+++ samples/ErrorProne.Samples/StructAnalyzers/TestReadOnlyMemory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErrorProne.Samples.StructAnalyzers
+{
+    public static class TestReadOnlyMemory
+    {
+        public static void WithReadOnlySequence(in ReadOnlyMemory<byte> r)
+        {
+
+        }
+    }
+}
