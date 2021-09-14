61d54636cdd28663fae7997ee8993007b34b92bd
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2021-09-14 13:48:44 -0700

    Implement DefaultToStringImplAnalyzer

---
+++ src/ErrorProne.NET.CoreAnalyzers.Tests/CoreAnalyzers/DefaultToStringConversionAnalyzerTests.cs
@@ -0,0 +1,166 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+using Verify = ErrorProne.NET.TestHelpers.CSharpCodeFixVerifier<
+    ErrorProne.NET.AsyncAnalyzers.DefaultToStringImplementationUsageAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace ErrorProne.NET.CoreAnalyzers.Tests.AsyncAnalyzers
+{
+    [TestFixture]
+    public class DefaultToStringConversionAnalyzerTests
+    {
+        [Test]
+        public async Task InterpolatedStringConversionWithMethodCall()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    Task FooBar() => null;
+    string T() => $""fb: {[|FooBar()|]}"";
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionWithMethodCallForTuple()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    (Task t1, int x) FooBar() => default;
+    string T() => $""fb: {[|FooBar()|]}"";
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionWithMethodCallForStruct()
+        {
+            var test = @"
+public struct Task {}
+class Test {
+    Task FooBar() => default;
+    string T() => $""fb: {[|FooBar()|]}"";
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionWithMethodCallAndLocal()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    Task FooBar() => null;
+    string T()
+    {
+        var fb = FooBar();
+        return $""fb: {[|fb|]}"";
+    }
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionWithVariable()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    string T(Task t) => $""t: {[|t|]}"";
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionInStringConcat()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    string T(Task t) => $""t"" + [|t|];
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionInStringConcat2()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    void T(Task t) => Foo($""t"" + [|t|]);
+    void Foo(string s) {}
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+
+        [Test]
+        public async Task InterpolatedStringConversionReturn()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    string T(Task t) => string.Format(""{0}"", [|t|]);
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task InterpolatedStringConversionReturnExplicit()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    string T(Task t) => string.Format(""{0}"", [|t.ToString()|]);
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task StringBuilderAppend()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    string T(Task t) => new System.Text.StringBuilder().Append([|t|]).ToString();
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+
+        [Test]
+        public async Task NoWarnOnConversionToObject()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    object T(Task t) => t;
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+        
+        [Test]
+        public async Task NoWarnOnConversionToObjectMethodCall()
+        {
+            var test = @"
+public class Task {}
+class Test {
+    void T(Task t) => Foo(t);
+    void Foo(object o) {}
+}
+";
+            await Verify.VerifyAsync(test);
+        }
+    }
+}
---
+++ src/ErrorProne.NET.CoreAnalyzers/CoreAnalyzers/DefaultToStringImplementationUsageAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using ErrorProne.NET.Core;
+using ErrorProne.NET.CoreAnalyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace ErrorProne.NET.AsyncAnalyzers
+{
+    /// <summary>
+    /// The analyzer warns when a task instance is implicitly converted to string.
+    /// </summary>
+    /// <remarks>
+    /// Here is an example: <code>var r = FooAsync(); Console.WriteLine($"r: {r}");</code>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DefaultToStringImplementationUsageAnalyzer : AbstractDefaultToStringImplementationUsageAnalyzer
+    {
+        /// <nodoc />
+        public static DiagnosticDescriptor Rule => DiagnosticDescriptors.EPC20;
+
+        /// <nodoc />
+        public DefaultToStringImplementationUsageAnalyzer()
+            : base(Rule)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override bool TryCreateDiagnostic(Compilation compilation, ITypeSymbol type, Location location, [NotNullWhen(true)]out Diagnostic? diagnostic)
+        {
+            diagnostic = null;
+            
+            if (NoToStringOverride(type, out var typeWithNoToString))
+            {
+                diagnostic = Diagnostic.Create(Rule, location, typeWithNoToString);
+            }
+
+            return diagnostic != null;
+        }
+
+        private static bool NoToStringOverride(ITypeSymbol type, [NotNullWhen(true)]out ITypeSymbol? typeWithNoToString)
+        {
+            typeWithNoToString = null;
+
+            if (type.IsTupleType && type is INamedTypeSymbol namedType)
+            {
+                // Getting the first type from the tuple with no ToString impl.
+                var noToString = namedType.TupleElements.Select(te => te.Type).Where(t => !t.OverridesToString());
+                typeWithNoToString = noToString.FirstOrDefault();
+            }
+            else
+            {
+                if (!type.OverridesToString())
+                {
+                    typeWithNoToString = type;
+                }
+            }
+
+            return typeWithNoToString != null;
+        }
+    }
+}
